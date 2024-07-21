07df806cbb0464fd4812986e05247fe90e7a4e9d
Halphane <ncdev@outlook.fr>
2024-07-21 16:03:25 +0200

    erased backend to restart from scracth

--- backend/backend/Context/DataContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace backend.Context;
-
-public class DataContext : DbContext
-{
-    public DataContext(DbContextOptions<DataContext> options): base(options) {}
-    
-}
