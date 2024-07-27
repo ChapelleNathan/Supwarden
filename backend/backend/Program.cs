bc1f912765b4d0d13f1f183b1d5fc3969ba8cf31
NathanChapelle <ncdev@outlook.fr>
2024-07-27 17:20:29 +0200

    added authorization in swagger

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -4,19 +4,41 @@ using backend.Repository.UserRepository;
 using backend.Services.AuthService;
 using backend.Services.UserServices;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddDbContext<DataContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection"))
 );
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "localhost", policy =>
+    {
+        policy.WithOrigins("http://localhost:8000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserServices, UserServices>();
